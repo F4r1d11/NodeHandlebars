9078dea13959a05dd06d12cdf76668365b616186
Farid <farid.delapena@gmail.com>
2019-08-16 14:13:42 -0500

    final commit

---
+++ db/seeds.sql
@@ -0,0 +1,7 @@
+INSERT INTO Burgers (id, name, devoured, date)
+VALUES
+	(1, 'Regular Hamburger', 1),
+	(2, 'Cheese Burger', 1),
+	(3, 'Ultra Bacon Burger', 0),
+	(4, 'Veggie Burger', 0),
+	(5, 'Chicken Club Burger', 0);
